47e7df262dbe8664c50dc216acc7263de2668034
AnuragDarji <anuragdarji29@gmail.com>
2025-06-21 13:13:15 +0530

    first commit

---
+++ Hello.js
@@ -0,0 +1,8 @@
+// const Math = require('./Math.js');
+
+// console.log(Math.add(5, 3));
+// console.log(Math.sub(5, 3));
+
+const { add, sub } = require('./Math.js');
+console.log(add(5, 3)); // Outputs: 8
+console.log(sub(5, 3)); // Outputs: 2
